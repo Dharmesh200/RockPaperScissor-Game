d0ebef5f5da6c1e7ffc2d821df480b3c04d52dfa
Dharmesh200 <dharmeshwaikar6@gmail.com>
2022-12-14 12:20:33 +0530

    adding comments into the code

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+//setting default values to score board
 let userScore = 0;
 let computerScore = 0;
+
+// fetching all element from html
 const userScore_span = document.getElementById('user-score');
 const computerScore_span = document.getElementById('computer-score');
 const scoreBoard_div = document.querySelector('.score-board');
@@ -13,12 +16,14 @@ const scissors_div = document.getElementById("siccor");
 const lizard_div = document.getElementById('lizard');
 const spock_div = document.getElementById('spock');
 
+//creating computer choices for rock papr scissor game
 function getComputerChoice() {
     const choices = ['rock', 'paper', 'siccor', 'lizard', 'spock'];
     const randomNumber = Math.floor(Math.random() * 5);
     return choices[randomNumber];
 };
 
+//creating win function it include all possible messages when user win and passing parameter as user choice and computer choice
 function win(userChoice, computerChoice) {
     console.log(userChoice, computerChoice);
     userScore++;
@@ -34,6 +39,7 @@ function win(userChoice, computerChoice) {
     computerSelect.innerHTML = `computer choose ${computerChoice}`;
 }
 
+//creating lose function it include all possible messages when user lose and passing parameter as user choice and computer choice
 function lose(userChoice, computerChoice) {
     console.log(userChoice, computerChoice);
     computerScore++;
@@ -50,6 +56,7 @@ function lose(userChoice, computerChoice) {
     document.querySelector('status').style.border = 'solid 1px red';
 }
 
+//creating draw function it include all possible messages when user draw and passing parameter as user choice and computer choice
 function draw(userChoice, computerChoice) {
     console.log(userChoice, computerChoice);
     statusResult.innerHTML = "Draw";
@@ -62,6 +69,7 @@ function draw(userChoice, computerChoice) {
     computerSelect.innerHTML = `computer choose ${computerChoice}`;
 };
 
+//creating result function which will display the result dynamically
 function displayResult() {
     resultBlock.style.display = 'flex';
     resultBlock.style.flexDirection = 'column';
@@ -73,9 +81,12 @@ function displayResult() {
 
 }
 
+// creating game function which store all the logic of the game
 function game(userChoice) {
     const computerChoice = getComputerChoice();
+    // user choice argument is comming from event listener which is present above the function
     switch (userChoice + computerChoice) {
+        //user winning logic
         case "rocksiccor":
         case "paperrock":
         case "siccorpaper":
@@ -85,6 +96,7 @@ function game(userChoice) {
             win(userChoice, computerChoice);
             break;
 
+        //user losing logic
         case "rockpaper":
         case "papersiccor":
         case "siccorrock":
@@ -94,6 +106,7 @@ function game(userChoice) {
             lose(userChoice, computerChoice);
             break;
 
+        //user draw logic
         case "rockrock":
         case "paperpaper":
         case "siccorsiccor":
@@ -103,8 +116,9 @@ function game(userChoice) {
             draw(userChoice, computerChoice);
             break;
     }
-}
+};
 
+// main function which hold all the events and passing second argument in eventlistner is game function.
 function main() {
     rock_div.addEventListener('click', () => game('rock'));
 
@@ -117,5 +131,4 @@ function main() {
     spock_div.addEventListener('click', () => game('spock')
     );
 };
-
 main();
